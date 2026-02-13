59173ffbaddde762269a054d9bc12200d34d7c26
Ram <imrbp.2k04@gmail.com>
2026-02-13 14:47:28 +0530

    feat: integrate scheduler module into app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { ObjectiveModule } from './objectives/objective.module';
 import { KeyResultsModule } from './key-results/key-results.module';
 import { ConfigModule } from '@nestjs/config';
 import AuthMiddleware from './common/middlewares/auth.middleware';
+import { SchedulerModule } from './scheduler/scheduler.module';
 
 @Module({
   imports: [
     ObjectiveModule,
     KeyResultsModule,
+    SchedulerModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
